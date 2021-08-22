4b590f7ff78fc000e3375f0edb7f4e73b1344a83
Debadyuti Banerjee <debadyuti.banerjee@google.com>
2021-08-23 00:19:50 +0530

    added pw project

---
+++ pw/src/app/app.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(event: any) {
+    const parsedValue = parseInt(event.target.value);
+    if ( !isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+  
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    // console.log('Button was clicked');
+    console.log(`
+      About to generate a password with the following:
+      Password Length: ${this.length}
+      Include letters: ${this.includeLetters}
+      Include numbers: ${this.includeNumbers}
+      Include symbols: ${this.includeSymbols}
+    `);
+
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters){
+      validChars += letters;
+    }
+    if (this.includeNumbers){
+      validChars += numbers;
+    }
+    if (this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  // getPassword() {
+  //   return this.password;
+  // }
+
+
+}
